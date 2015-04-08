77b95b7e02d99213a3e2042895a862edd550d733
victor@uniq.by <victor@uniq.by>
2015-04-08 17:56:59 +0300

    Fixes #3652 @0.5h

--- components/helpers/Helpers.php
+++ components/helpers/Helpers.php
@@ -4,7 +4,10 @@ namespace app\components\helpers;
 class Helpers {
 
     //Round numbers
-    public static function roundUp($number, $accuracy = 50) {
+    public static function roundUp($number, $accuracy = 1)
+    {
+
+        $accuracy = \Yii::$app->settings->get('main.roundAccuracy') ?: $accuracy;
         if ($accuracy != 1)
             $number = ceil($number / $accuracy) * $accuracy;
         return $number;
