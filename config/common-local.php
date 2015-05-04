5f121057ca5db107baf2397b0f16e0b1f4d776a8
victor@uniq.by <victor@uniq.by>
2015-05-04 10:50:47 +0300

    config on prod

--- config/common-local.php
+++ config/common-local.php
@@ -2,12 +2,12 @@
 return [
     'components' => [
         'db' => [
-//            'dsn' => 'mysql:host=178.62.250.164;dbname=avtosm_avtosm_crm',
-//            'username' => 'avtosm_avtosm',
-//            'password' => 'V4s3tmPBI9',
-            'dsn' => 'mysql:host=localhost;dbname=avtosm_avtosm_crm',
-            'username' => 'root',
-            'password' => 'root',
+            'dsn' => 'mysql:host=178.62.250.164;dbname=avtosm_avtosm_crm',
+            'username' => 'avtosm_avtosm',
+            'password' => 'V4s3tmPBI9',
+//            'dsn' => 'mysql:host=localhost;dbname=avtosm_avtosm_crm',
+//            'username' => 'root',
+//            'password' => 'root',
         ],
         'mailer' => [
             'useFileTransport' => true,
--- web/index.php
+++ web/index.php
@@ -3,9 +3,10 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'prod');
-//if (YII_ENV_PROD)
-//        error_reporting(E_NONE);
-error_reporting(false);
+if (YII_ENV_PROD) {
+    error_reporting(E_NONE);
+}
+//error_reporting(false);
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
